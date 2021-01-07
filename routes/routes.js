dea63086548a4d6666ef528d75d866408fdc5726
Dev-777 <arsen172317@gmail.com>
2021-01-07 21:06:40 +0400

    send email & approve code => done

--- routes/routes.js
+++ routes/routes.js
@@ -2,25 +2,58 @@ const express = require("express");
 const router = express.Router();
 const signupTemplateCopy = require("../models/SignUpModels");
 const bcrypt = require("bcrypt");
+const nodemailer = require("nodemailer");
 
-router.post("/signup", async (req, res) => {
-  const saltPassword = await bcrypt.genSalt(10);
-  const securePassword = await bcrypt.hash(req.body.password, saltPassword);
+let code = null;
+let user = {};
 
-  const signedUpUser = new signupTemplateCopy({
-    name: req.body.name,
-    username: req.body.username,
-    email: req.body.email,
-    password: securePassword,
+const sendMailFunc = (email, code) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "arsen.ghazaryanT@gmail.com",
+      pass: "Aa!234567890",
+    },
   });
-  signedUpUser
-    .save()
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((error) => {
-      res.json(error);
+  const mailOptions = {
+    from: "arsen.ghazaryanT@gmail.com",
+    to: email,
+    subject: "Sending Email using Node.js",
+    text: `this is the approve code\` ${code}`,
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+};
+
+router.post("/approve", async (req, res) => {
+  if (code === +req.body.code) {
+    const saltPassword = await bcrypt.genSalt(10);
+    const securePassword = await bcrypt.hash(user.password, saltPassword);
+
+    const signedUpUser = new signupTemplateCopy({
+      name: user.name,
+      username: user.username,
+      email: user.email,
+      password: securePassword,
     });
+    signedUpUser
+      .save()
+      .then(() => console.log("data added~!"))
+      .catch((error) => {
+        res.json(error);
+      });
+  }
+});
+
+router.post("/signup", (req, res) => {
+  user = req.body;
+  code = Math.floor(Math.random() * 1000);
+  sendMailFunc(req.body.email, code);
 });
 
 router.get("/data17", (req, res) => {
