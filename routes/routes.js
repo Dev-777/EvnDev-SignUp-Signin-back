9b8dfa744f7363692e6263862132593269e5062f
Dev-777 <arsen172317@gmail.com>
2021-01-07 14:27:03 +0400

    s

--- routes/routes.js
+++ routes/routes.js
@@ -2,57 +2,25 @@ const express = require("express");
 const router = express.Router();
 const signupTemplateCopy = require("../models/SignUpModels");
 const bcrypt = require("bcrypt");
-const nodemailer = require("nodemailer");
 
-let code = 15;
-let user;
+router.post("/signup", async (req, res) => {
+  const saltPassword = await bcrypt.genSalt(10);
+  const securePassword = await bcrypt.hash(req.body.password, saltPassword);
 
-const sendMailFunc = (email, code) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: "arsen.ghazaryanT@gmail.com",
-      pass: "Aa!234567890",
-    },
+  const signedUpUser = new signupTemplateCopy({
+    name: req.body.name,
+    username: req.body.username,
+    email: req.body.email,
+    password: securePassword,
   });
-  const mailOptions = {
-    from: "arsen.ghazaryanT@gmail.com",
-    to: email,
-    subject: "Sending Email using Node.js",
-    text: `this is the approve code\` ${code}`,
-  };
-  transporter
-    .sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log("Email sent: " + info.response);
-      }
+  signedUpUser
+    .save()
+    .then((data) => {
+      res.json(data);
     })
-    .then((r) => console.log(r));
-};
-
-router.post("/approve", async (req, res) => {
-  if (code === +res.code) {
-    const saltPassword = await bcrypt.genSalt(10);
-    const securePassword = await bcrypt.hash(user.password, saltPassword);
-
-    const signedUpUser = new signupTemplateCopy({
-      name: user.name,
-      username: user.username,
-      email: user.email,
-      password: securePassword,
-    });
-    signedUpUser.save().catch((error) => {
+    .catch((error) => {
       res.json(error);
     });
-  }
-});
-
-router.post("/signup", (req, res) => {
-  user = req;
-  code = Math.floor(Math.random() * 10);
-  sendMailFunc(req.email, code);
 });
 
 router.get("/data17", (req, res) => {
