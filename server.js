3aae96de437b019acf48ff103772d12de4e5688b
Dev-777 <arsen172317@gmail.com>
2021-01-10 00:36:57 +0400

    dd

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const routesUrls = require("./routes/routes");
-const cors = require("cors");
+// const cors = require("cors");
 const PORT = process.env.PORT || 4000;
 
 dotenv.config();
@@ -13,8 +13,18 @@ mongoose.connect(
   () => console.log("database connected!!!")
 );
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 app.use("/app", routesUrls);
 
 app.listen(PORT, () => console.log("server is up"));
