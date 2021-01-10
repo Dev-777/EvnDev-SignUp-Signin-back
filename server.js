9850daaef8fb133f9e4296222809339938c11b30
Dev-777 <arsen172317@gmail.com>
2021-01-11 02:24:07 +0400

    vvv

--- server.js
+++ server.js
@@ -4,8 +4,19 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const routesUrls = require("./routes/routes");
 const PORT = process.env.PORT || 4000;
-const cors = require("cors");
-app.use(cors());
+// const cors = require("cors");
+// app.use(cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
 
 dotenv.config();
 mongoose.connect(
