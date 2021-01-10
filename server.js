f96dddc2866269b2cd3eff97f2528ede5428b78b
Dev-777 <arsen172317@gmail.com>
2021-01-11 00:05:06 +0400

    ddll

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const routesUrls = require("./routes/routes");
 const PORT = process.env.PORT || 4000;
+const cors = require("cors");
+app.use(cors());
 
 dotenv.config();
 mongoose.connect(
